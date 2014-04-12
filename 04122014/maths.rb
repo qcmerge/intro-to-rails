52b5ed7dc31356f0af6d56e19668a00c4a2113fb
Ben Stafford <bendstafford@gmail.com>
2014-04-12 10:29:47 -0400

    Update maths.rb with other example and annotate with some comments

--- 04122014/maths.rb
+++ 04122014/maths.rb
@@ -1,6 +1,11 @@
+# To check results
+# At comand line in same directory, run with ruby: "ruby maths.rb"
+# Start IRB in same directory and then type: "require './maths'" and press enter to load the file into
+# the IRB session
+
  # Trivial class that implements some binary math operations explicitly
  class BinaryOperations
-  def self.add(x,y)
+  def self.add(x, y)
     x + y
   end
 
@@ -13,16 +18,35 @@
   end
 end
 
-# Class that provides support for binary operations as block method
+# Class that provides support for ANY binary operation as block method
 class BlockBinaryOperations
-  def self.perform(x, y, &block)
-    yield(x, y)
+  #block defines operation to be performed on x and y
+  def self.perform(x, y)
+    # if block is supplied yield with two params, otherwise just do addition
+    if block_given?
+      yield(x, y)
+    else
+      x + y
+    end
   end
 end
 
+# Examples of how to call methods in the non-blockified BinaryOperations class
 puts "Non-block: #{BinaryOperations.add(2, 5)}"
+puts "Non-block: #{BinaryOperations.subtract(2, 5)}"
 
+
+# call BlockBinaryOperations.perform with addition defined in the block.
 result = BlockBinaryOperations.perform(3, 5) do |x, y|
   x + y
 end
 puts "With block: #{result}"
+
+# Pass another block to BlockBinaryOperations.perform with code that isn't defined there.
+# (in this case, passing in code that divides x and y)
+BlockBinaryOperations.perform(3, 5) do |x, y|
+  x / y
+end
+
+
+
